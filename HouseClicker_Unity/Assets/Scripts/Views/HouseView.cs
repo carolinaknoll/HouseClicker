f7db04824f9cf982e3ea5a8c97be3a04a061076f
Willian Correa <willian.cs@gmail.com>
2019-04-14 12:24:07 -0300

    Add HouseView script (WIP)

---
+++ HouseClicker_Unity/Assets/Scripts/Views/HouseView.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class HouseView : MonoBehaviour
+{
+    public RawImage HouseRawImage;
+    public Button HouseButton;
+
+    public void Start()
+    {
+        HouseButton
+            .OnClickAsObservable()
+            .Subscribe(_ => { Debug.LogError("Wololo"); });
+    }
+}
