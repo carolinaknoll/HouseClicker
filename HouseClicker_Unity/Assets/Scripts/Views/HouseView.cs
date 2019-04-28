dcf9e48bb6d4b0147b8a09e371f1f4a02d487da5
Willian Correa <willian.cs@gmail.com>
2019-04-28 18:11:56 -0300

    Add house repair slider (WIP)

--- HouseClicker_Unity/Assets/Scripts/Views/HouseView.cs
+++ HouseClicker_Unity/Assets/Scripts/Views/HouseView.cs
@@ -6,11 +6,16 @@ public class HouseView : MonoBehaviour
 {
     public RawImage HouseRawImage;
     public Button HouseButton;
+    public Slider HouseSlider;
 
     public void Start()
     {
         HouseButton
             .OnClickAsObservable()
-            .Subscribe(_ => { Debug.LogError("Wololo"); });
+            .Subscribe(_ => 
+            {
+                Debug.LogError("Wololo");
+                HouseSlider.value += 0.01f;
+            });
     }
 }
