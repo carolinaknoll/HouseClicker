8d84eb1e7e460babfa920ab0deaf3195817f1831
Willian Correa <willian.cs@gmail.com>
2019-05-04 23:47:58 -0300

    Add TextMeshPro and its component to main scene as main score

--- HouseClicker_Unity/Assets/Scripts/Views/HouseView.cs
+++ HouseClicker_Unity/Assets/Scripts/Views/HouseView.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
+using TMPro;
 
 public class HouseView : MonoBehaviour
 {
@@ -8,14 +9,22 @@ public class HouseView : MonoBehaviour
     public Button HouseButton;
     public Slider HouseSlider;
 
+    public TextMeshProUGUI TotalRepairPointsText;
+
+    private float _totalRepairPoints;
+
     public void Start()
     {
         HouseButton
             .OnClickAsObservable()
             .Subscribe(_ => 
             {
+                _totalRepairPoints += 0.01f;
+
                 Debug.LogError("Wololo");
                 HouseSlider.value += 0.01f;
+
+                TotalRepairPointsText.text = string.Format("{0:#0.00}", _totalRepairPoints);
             });
     }
 }
